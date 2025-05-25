ed413ef0515dfdc60b0980219df9d32806dc8392
xy2dxdy <sashamilto3@gmail.com>
2025-05-25 21:04:05 +0300

    Revert "Exp_Bzr"
    This reverts commit 41cecf626fb6812cfe7707f689aa47008e3c79d7.

--- Assets/Scripts/Enemy/Health/EnemyHealth.cs
+++ Assets/Scripts/Enemy/Health/EnemyHealth.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour, IHealth
 {
+
     [SerializeField] private EnemyConfig _config;
 
     public event Action OnDecrease;
@@ -23,31 +24,15 @@ public class EnemyHealth : MonoBehaviour, IHealth
 
     public void Decrease(float value)
     {
-        if (IsDead) return;
-
         Current -= value;
 
         if (Current <= _config.MinHealthPoints)
         {
-            Die();
+            Current = _config.MinHealthPoints;
+            IsDead = true;
         }
 
         OnDecrease?.Invoke();
-    }
-
-    private void Die()
-    {
-        IsDead = true;
-        Current = _config.MinHealthPoints;
-
-        
-        PlayerExperience playerExp = FindObjectOfType<PlayerExperience>();
-        if (playerExp != null)
-        {
-            playerExp.Increase(_config.ExperienceReward);
-        }
-
-        Destroy(gameObject);
     }
 
     public void Increase(float value)
